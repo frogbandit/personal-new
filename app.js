c9b7e26b813f5be79b7221182f97fb3c57b5dabf
James Xue <jamesxue100@gmail.com>
2016-07-30 03:48:32 -0700

    added projects page

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.get('/',function(req, res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+app.get('/projects',function(req, res){
+  res.sendFile(path.join(__dirname+'/public/projects.html'));
 });
 
 var listener = server.listen(process.env.PORT || 3000, function () {
